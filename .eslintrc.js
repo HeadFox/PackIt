e642a8d92f97d7592507d6fa41818ef59f36483d
Lucien PESLIER <lucien.headfox@gmail.com>
2017-07-03 21:59:50 +0200

    fix: Fix unresolved path alias #2

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,23 @@
 module.exports = {
-    "extends": "airbnb",
-    "plugins": [
-        "react",
-        "jsx-a11y",
-        "import"
-    ],
-    "rules": {
-      "quotes": ["error", "double"]
+  "extends": "airbnb",
+  "plugins": [
+    "react", "jsx-a11y", "import",
+  ],
+  "rules": {
+    "quotes": ["error", "double"],
+  },
+  "settings": {
+    "import/resolver": {
+      "webpack": {
+        "config": "./config/config.js",
+        "commons": "./src/components/commons",
+        "images": "./src/assets/images",
+      },
     },
-    "env": {
-        "browser": true,
-        "es6": true,
-        "node": true
-    }
+  },
+  "env": {
+    "browser": true,
+    "es6": true,
+    "node": true,
+  },
 };
